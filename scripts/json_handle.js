8c9862f06ba94f9b14e7742bb3bacd368f1320d4
YagiGo <jeremywu1995@gmail.com>
2018-04-19 14:58:17 +0900

    Handle local file with XMLHTTPRequest

--- scripts/json_handle.js
+++ scripts/json_handle.js
@@ -14,3 +14,16 @@ function showMeJson() {
 
     }
 }
+function readJSONFile(file) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open('GET', file, false);
+    rawFile.onreadystatechange = function() {
+        if(rawFile.readyState == 4) {
+            if(rawFile.status === 200 || rawFile.status == 0) {
+                var allText = rawFile.responseText;
+                //convert to JSON object
+                var JSONText = JSON.parse(allText);
+            }
+        }
+    }
+}
